5131440332ca381df32751b9447b0ea9aef9b263
清水孔太 <ba07bo30lat@icloud.com>
2024-06-10 22:10:52 +0900

    掲示板の骨格HTML、CSSを作成

--- index.php
+++ index.php
@@ -1,2 +1,47 @@
-<?php
-echo "Hello PHP";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>２ちゃんねる掲示板</title>
+  <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+  <header>
+    <h1 class="title">２ちゃんねる掲示板</h1>
+    <hr>
+  </header>
+
+  <!-- スレッドエリア -->
+   <div class="threadWrapper">
+    <div class="childWrapper">
+      <div class="threadTitle">
+        <span>【タイトル】</span>
+        <h1>２ちゃんねる掲示板をつくってみたお（・ｖ・）</h1>
+      </div>
+      <section>
+        <article>
+          <div class="wrapper">
+            <div class="nameArea">
+              <span>名前：</span>
+              <p class="username">kota</p>
+              <time>：2022/7/16 12:00</time>
+            </div>
+            <p class="comment">手書きのコメントです</p>
+          </div>
+        </article>
+      </section>
+      <form class="formWrapper">
+        <div>
+          <input type="submit" value="書き込む">
+          <label>名前：</label>
+          <input type="text">
+        </div>
+        <div>
+          <textarea class="commentTextArea"></textarea>
+        </div>
+      </form>
+    </div>
+   </div>
+</body>
+</html>
